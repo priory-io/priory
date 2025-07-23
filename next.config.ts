dfe884cee326cab9a647dd8fe2ea799936d8c082
Keiran <keircn@proton.me>
2025-07-23 21:01:51 +0100

    next config stuff

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const isProd: boolean = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: isProd,
+  },
+  eslint: {
+    ignoreDuringBuilds: isProd,
+  },
+  typescript: {
+    ignoreBuildErrors: isProd,
+  },
+  experimental: {
+    reactCompiler: isProd,
+    cssChunking: isProd,
+  }
 };
 
 export default nextConfig;
