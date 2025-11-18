8d0d71af370331224ef11f93c73213bc1db9fb24
Keiran <keircn@proton.me>
2025-11-18 09:18:52 +0000

    rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const headers = new Headers(request.headers);
   headers.set("x-pathname", request.nextUrl.pathname);
 
